e042345679e88e11678dfbed079bff766f48a778
Andrew Davis <develop.it4@gmail.com>
2024-05-02 12:29:45 -0400

    clean up styling on individual repo view

--- app/components/navList.tsx
+++ app/components/navList.tsx
@@ -2,34 +2,69 @@ import { NavLink } from '@remix-run/react';
 
 export default function NavList(): JSX.Element {
   return (
-    <>
-      <NavLink
-        to="/repos/javascript"
-        className={({ isActive, isPending }) =>
-          isPending ? 'pending' : isActive ? 'active' : ''
-        }
-      >
-        JS
-      </NavLink>
-      
-      <NavLink
-        to="/repos/python"
-        className={({ isActive, isPending }) =>
-          isPending ? 'pending' : isActive ? 'active' : ''
-        }
-      >
-        Python
-      </NavLink>
-    
-      <NavLink
-        to="/repos/rust"
-        className={({ isActive, isPending }) =>
-          isPending ? 'pending' : isActive ? 'active' : ''
-        }
-      >
-        Rust
-      </NavLink>
-      
-    </>
+    <nav>
+      <ul className="flex flex-row justify-center">
+        <li className="m-2">
+          <NavLink
+            to="/repos/all"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            All
+          </NavLink>
+        </li>
+        <li className="m-2">
+          <NavLink
+            to="/repos/javascript"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            JavaScript
+          </NavLink>
+        </li>
+        <li className="m-2">
+          <NavLink
+            to="/repos/java"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            Java
+          </NavLink>
+        </li>
+        <li className="m-2">
+          <NavLink
+            to="/repos/python"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            Python
+          </NavLink>
+        </li>
+        <li className="m-2">
+          <NavLink
+            to="/repos/rust"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            Rust
+          </NavLink>
+        </li>
+        <li className="m-2">
+          <NavLink
+            to="/repos/css"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'font-bold' : ''
+            }
+          >
+            CSS
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
