89c79d0e7dfbfbd989986bd42dfb8f3fd717a63e
Andrew Davis <develop.it4@gmail.com>
2024-05-06 16:27:01 -0400

    more api utils

--- app/routes/battle.tsx
+++ app/routes/battle.tsx
@@ -1,3 +1,15 @@
+import { ActionFunctionArgs, json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { getProfile, getRepos, getUserData } from './utils';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+export const loader = async () => {
+  const p = await getUserData('quincylarson');
+  console.log(p)
+  return {};
+};
+
 export default function Battle() {
   return (
     <div>
@@ -9,18 +21,42 @@ export default function Battle() {
       </div>
 
       <div className="flex justify-around mt-[50px]">
-        <form action="">
+        <form method="post">
           <label htmlFor="playerOne">Player One</label>
           <div className="flex">
-            <input type="text" name="playerOne" id="playerOne" className="flex-1" />
-            <button className="bg-[rebeccapurple] text-white p-2">Submit</button>
+            <input
+              type="text"
+              name="playerOne"
+              id="playerOne"
+              className="flex-1"
+            />
+            <button
+              className="bg-[rebeccapurple] text-white p-2"
+              type="submit"
+              name="_action"
+              value="playerOne"
+            >
+              Submit
+            </button>
           </div>
         </form>
-        <form action="">
+        <form method="post">
           <label htmlFor="playerTwo">Player Two</label>
           <div className="flex">
-            <input type="text" name="playerTwo" id="playerTwo" className="flex-1" />
-            <button className="bg-[rebeccapurple] text-white flex-1 p-2">Submit</button>
+            <input
+              type="text"
+              name="playerTwo"
+              id="playerTwo"
+              className="flex-1"
+            />
+            <button
+              className="bg-[rebeccapurple] text-white flex-1 p-2"
+              type="submit"
+              name="_action"
+              value="playerTwo"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
--- app/routes/utils.ts
+++ app/routes/utils.ts
@@ -14,3 +14,46 @@ export async function searchRepos(language: string) {
     throw new Error((error as Error).message);
   }
 }
+
+export async function getRepos(username: string) {
+  const url = `https://api.github.com/users/${username}/repos`;
+  const request = await octokit.request(`GET ${url}`, {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+  return request.data;
+}
+
+export async function getProfile(username: string) {
+  const url = `https://api.github.com/users/${username}`;
+  const request = await octokit.request(`GET ${url}`, {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+
+  return request.data;
+}
+
+export async function getUserData(player: string) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getStarCount(repos: any) {
+  return repos.reduce(
+    (count: number, { stargazers_count }: { stargazers_count: number }) =>
+      count + stargazers_count,
+    0
+  );
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos);
+}
