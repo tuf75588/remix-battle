e3351505f517e9ec4677ac27be05ce2c3c9f2dbc
Andrew Davis <develop.it4@gmail.com>
2024-05-17 20:11:37 -0400

    start fresh with battle route

--- app/routes/battle.tsx
+++ app/routes/battle.tsx
@@ -1,27 +1,15 @@
-import { ActionFunctionArgs, json } from '@remix-run/node';
+import {  ActionFunctionArgs, json } from '@remix-run/node';
 import { useLoaderData, useActionData } from '@remix-run/react';
 import { getRepos, getUserData } from './utils';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 //
 
-export const action = async ({ request }: ActionFunctionArgs) => {
-  const formData = await request.formData();
-  const { _action, ...values } = Object.fromEntries(formData);
-  if (_action === 'playerOne') {
-    let p1 = await getUserData(values.playerOne, 1);
-    return p1;
-  }
-  if (_action === 'playerTwo') {
-    let p2 = await getUserData(values.playerTwo, 2);
-    return p2;
-  }
-  return {};
-};
-
+export const action = async ( { request }: ActionFunctionArgs) => {
+  console.log(request.url);
+  return null;
+}
 export default function Battle() {
-  const data = useActionData<typeof action>();
-  console.log(data);
   return (
     <div>
       <h1 className="text-4xl text-center font-normal">Instructions</h1>
@@ -32,43 +20,10 @@ export default function Battle() {
       </div>
 
       <div className="flex justify-around mt-[50px]">
-        {data && (data as { position: number }).position === 1 ? (
-          <p>one</p>
-        ) : (
-          <form method="post">
-            <label htmlFor="playerOne">Player One</label>
-            <div className="flex">
-              <input type="text" name="playerOne" className="flex-1" />
-              <button
-                className="bg-[rebeccapurple] text-white p-2"
-                type="submit"
-                name="_action"
-                value="playerOne"
-              >
-                Submit
-              </button>
-              <input type="hidden" value="playerOne" />
-            </div>
-          </form>
-        )}
-        {data && (data as { position: number }).position === 2 ? (
-          <p>two</p>
-        ) : (
-          <form method="post">
-            <label htmlFor="playerTwo">Player Two</label>
-            <div className="flex">
-              <input type="text" name="playerTwo" className="flex-1" />
-              <button
-                className="bg-[rebeccapurple] text-white flex-1 p-2"
-                type="submit"
-                name="_action"
-                value="playerTwo"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
+        <p>this is where the players will go</p>
+        <form method="post">
+          <button type="submit" name="submit">submit</button>
+        </form>
       </div>
     </div>
   );
