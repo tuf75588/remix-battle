a07eccb1967ce6ad01c5462692f94eb2cd91e063
Andrew Davis <develop.it4@gmail.com>
2024-05-22 12:35:37 -0400

    break out files into new components

---
+++ app/components/game-form.tsx
@@ -0,0 +1,11 @@
+import { Form, useFetcher } from "@remix-run/react";
+
+export default function GameForm() {
+  const fetcher = useFetcher();
+
+  return (
+    <div>
+        GAME FORM
+    </div>
+  );
+}
--- app/routes/battle.tsx
+++ app/routes/battle.tsx
@@ -1,14 +1,14 @@
-import {  ActionFunctionArgs, json } from '@remix-run/node';
+import { ActionFunctionArgs, json } from '@remix-run/node';
 import { useLoaderData, useActionData } from '@remix-run/react';
 import { getRepos, getUserData } from './utils';
+import GameForm from '~/components/game-form';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//
 
-export const action = async ( { request }: ActionFunctionArgs) => {
-  console.log(request.url);
+export const loader = async () => {
   return null;
-}
+};
+
 export default function Battle() {
   return (
     <div>
@@ -18,12 +18,8 @@ export default function Battle() {
         <div>battle</div>
         <div>see the winner</div>
       </div>
-
-      <div className="flex justify-around mt-[50px]">
-        <p>this is where the players will go</p>
-        <form method="post">
-          <button type="submit" name="submit">submit</button>
-        </form>
+      <div className="flex justify-evenly mt-[50px]">
+        <GameForm />
       </div>
     </div>
   );
